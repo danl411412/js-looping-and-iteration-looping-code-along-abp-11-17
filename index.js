14a6e46f68a653b9f5d38d278760202bed316d61
danl411412 <daan.b.lee@gmail.com>
2017-11-17 15:53:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function printBadges(arr) {
   }
   return arr;
 }
+
+function tailsNeverFails() {
+  let counter = 0;
+  while(Math.random >= 0.5) {
+    counter++;
+    console.log(`You got ${counter} tails in a row!`);
+  }
+}
